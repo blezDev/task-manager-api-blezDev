0d24d8357a80e1c3980922bf385fa7ed902ce998
M Saikrishna Pattnaik <msaikrishnapatnaik@gmail.com>
2026-02-03 18:01:48 +0530

    Added sorted code

--- controlller/task.controller.js
+++ controlller/task.controller.js
@@ -2,7 +2,7 @@ const {TaskModel} = require('../models/task.model');
 const asyncHandler = require('express-async-handler');
 
 const allTasks = asyncHandler(async (req, res) => {
-    const tasks = await TaskModel.find({});
+    const tasks = await TaskModel.find({}).sort({createdAt: -1, updatedAt: -1});
     res.status(200).json({success: true, data: tasks});
 });
 
@@ -67,18 +67,18 @@ const updateTask = asyncHandler(async (req, res) => {
 });
 
 const deleteTask = asyncHandler(async (req, res) => {
-   const reqParams = req.params;
-   const {id} = reqParams;
+    const reqParams = req.params;
+    const {id} = reqParams;
 
-   const isPresent = await TaskModel.findById(id);
+    const isPresent = await TaskModel.findById(id);
 
-   if(!isPresent){
-       const err = new Error(`Task with id ${id} not found`);
-       err.statusCode = 404;
-       throw err;
-   }
-   await TaskModel.findByIdAndDelete(id);
-   res.status(200).json({success:true, message: `Task with id ${id} has been deleted successfully`});
+    if (!isPresent) {
+        const err = new Error(`Task with id ${id} not found`);
+        err.statusCode = 404;
+        throw err;
+    }
+    await TaskModel.findByIdAndDelete(id);
+    res.status(200).json({success: true, message: `Task with id ${id} has been deleted successfully`});
 });
 
 const saveTask = asyncHandler(async (req, res) => {
@@ -89,8 +89,6 @@ const saveTask = asyncHandler(async (req, res) => {
     res.status(201).json({success: true, data: createTask});
 
 });
-
-
 
 
 module.exports = {allTasks, taskById, taskByPriority, saveTask, updateTask, deleteTask, taskByTitleOrDescription};
